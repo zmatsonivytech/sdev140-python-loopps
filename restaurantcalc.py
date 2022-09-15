0b8a2d5b11de52306b20fe9c5cb70618d765b43a
zmatsonivytech <113141386+zmatsonivytech@users.noreply.github.com>
2022-09-15 15:26:49 -0400

    initial

---
+++ restaurantcalc.py
@@ -0,0 +1,3 @@
+charge = input("Cost of food")
+total = float(charge) + (float(charge) * .18) + (float(charge) * .07)
+print(total)
---
+++ temp.py
@@ -0,0 +1,4 @@
+celsius = input("Celsius: ")
+
+fahrenheit = 9/5*int(celsius)+32
+print(fahrenheit)
